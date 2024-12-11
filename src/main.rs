3bb411d68c8c825602e7cb848dcab93e3b9891df
Vadim Ponomarev <38704817+VxDxK@users.noreply.github.com>
2024-12-11 23:50:03 +0300

    Add header with sha256 sum of body, and add simple to cli to configure port and address (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::{borrow::Cow, net::SocketAddr, sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex}};
+use std::{
+    borrow::Cow,
+    net::SocketAddr,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, Mutex,
+    },
+};
 
+use axum::http::{HeaderMap, HeaderValue};
+use axum::response::IntoResponse;
 use axum::{extract::Path, http::StatusCode, response::Html, routing::get, Router};
 use axum_server::tls_rustls::RustlsConfig;
-use rand::{thread_rng, Rng};
-use tokio::time::{interval, Duration};
-use tracing::info;
-use std::net::TcpListener;
-use tower_http::trace::TraceLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use clap::Parser;
 use once_cell::sync::Lazy;
+use rand::{thread_rng, Rng};
+use sha2::{Digest, Sha256};
+use std::net::TcpListener;
+use tokio::time::{interval, Duration};
+use tower_http::trace::TraceLayer;
+use tracing::{info, warn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    #[arg(short, long, default_value = "127.0.0.1")]
+    address: std::net::IpAddr,
+
+    #[arg(short, long, default_value = "3000")]
+    port: u16,
+}
 
 const MAX_BYTES_LIMIT: usize = 10_000_000;
 
 static RANDOM_BYTES: Lazy<RandomDataBuffer> = Lazy::new(RandomDataBuffer::new);
-
 
 struct RandomDataBuffer {
     data: Vec<u8>,
@@ -37,7 +57,7 @@ impl RandomDataBuffer {
         if *position + size < MAX_BYTES_LIMIT {
             let slice = &self.data[*position..*position + size];
 
-            *position = (*position + size) & MAX_BYTES_LIMIT;
+            *position = (*position + size) % MAX_BYTES_LIMIT;
 
             Cow::Borrowed(slice)
         } else {
@@ -47,7 +67,7 @@ impl RandomDataBuffer {
             result.extend_from_slice(&self.data[*position..]);
             result.extend_from_slice(&self.data[..size - first_part_size]);
 
-            *position = (*position + size) & MAX_BYTES_LIMIT;
+            *position = (*position + size) % MAX_BYTES_LIMIT;
             Cow::Owned(result)
         }
     }
@@ -73,13 +93,13 @@ impl RequestCounter {
     }
 }
 
-
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
     // Initialize tracing
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into())
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -107,22 +127,23 @@ async fn main() {
         }
     });
 
-
     // Build application with necessary routes
     let app = Router::new()
         .route("/", get(root))
-        .route("/bytes/:n", get(move |path| rand_bytes(path, counter.clone())))
+        .route(
+            "/bytes/:n",
+            get(move |path| rand_bytes(path, counter.clone())),
+        )
         .layer(TraceLayer::new_for_http());
 
-    let addr = "127.0.0.1:3000".parse::<SocketAddr>().unwrap();
+    let addr = SocketAddr::new(cli.address, cli.port);
     info!("listenning on {}", addr);
 
     let listener = TcpListener::bind(addr).unwrap();
 
-    let config = RustlsConfig::from_pem_file(
-        "keys/server.crt",
-        "keys/server.key"
-    ).await.unwrap();
+    let config = RustlsConfig::from_pem_file("keys/server.crt", "keys/server.key")
+        .await
+        .unwrap();
 
     axum_server::from_tcp_rustls(listener, config)
         .serve(app.into_make_service())
@@ -131,19 +152,31 @@ async fn main() {
 }
 
 async fn root() -> Html<&'static str> {
-    Html("<h1>Welcome to Random Bytes Server.</h1>
-          <p>Use the /bytes/N endpoint to get N random bytes.</p>")
+    Html(
+        "<h1>Welcome to Random Bytes Server.</h1>
+          <p>Use the /bytes/N endpoint to get N random bytes.</p>",
+    )
 }
 
-async fn rand_bytes<'a>(
-    Path(n): Path<usize>,
-    counter: Arc<RequestCounter>
-) -> Result<Cow<'a, [u8]>, StatusCode> {
+async fn rand_bytes<'a>(Path(n): Path<usize>, counter: Arc<RequestCounter>) -> impl IntoResponse {
     counter.increment();
 
     if n > MAX_BYTES_LIMIT {
         return Err(StatusCode::BAD_REQUEST);
     }
-
-    Ok(RANDOM_BYTES.get_slice(n))
+    let slice = RANDOM_BYTES.get_slice(n);
+    let mut hasher = Sha256::new();
+    hasher.update(&slice);
+    let hash = hasher.finalize();
+    let hex_hash = base16ct::lower::encode_string(&hash);
+    let mut headers = HeaderMap::new();
+    match HeaderValue::from_str(&hex_hash) {
+        Ok(value) => {
+            headers.insert("sha256", value);
+        }
+        Err(e) => {
+            warn!("err{e:?}");
+        }
+    };
+    Ok((headers, slice))
 }
